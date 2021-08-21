ddf17991e3b642d3c53e7245e919c96c0734ad22
Christian Vaughn <cvaughn55@gmail.com>
2021-08-21 12:53:02 -0700

    Add short test
    added a quick test to see if function ran without errors. need to do graph to view results

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def ojas(X, learning_rate = 0.1, iters = 100, features = 3):
+def ojas(X, learning_rate = 0.1, iters = 100, features = 2):
     #create weights array initialized to random values
     w = np.random.rand(1,features)
     #w = np.array([0.5,0.5])
@@ -12,6 +12,16 @@ def ojas(X, learning_rate = 0.1, iters = 100, features = 3):
     # Train - Fit
     for i in range(1,50):
         y = np.dot(X,w.T)
-        delta_w = (np.multiply(y,X)) - (learning_rate * np.dot((np.square(y)),w))
+        delta_w = (learning_rate * np.multiply(y,X)) - (learning_rate * np.dot((np.square(y)),w))
         w = w + np.sum(delta_w)
     return w
+
+#TEST OJAS Rule
+# Create Data
+x = 0.5 * np.random.normal(size=10)
+y = 0.5 * np.random.normal(size=10)
+X = np.array([x , y]).T
+X.shape
+
+W = ojas(X)
+print(W)
